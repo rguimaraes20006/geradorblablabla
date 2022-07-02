e7213dc556a5939776d717e9aab1108518b58494
Rodrigo Guimarães <rguimaraes@ons.org.br>
2022-07-01 22:03:07 -0300

    Inicial

---
+++ descricaoaleatoria/lib/randomadvice.dart
@@ -0,0 +1,71 @@
+import 'package:dio/dio.dart';
+
+
+
+// To parse this JSON data, do
+//
+//     final randomAdvice = randomAdviceFromJson(jsonString);
+
+import 'dart:convert';
+
+RandomAdvice randomAdviceFromJson(String str) => RandomAdvice.fromJson(json.decode(str));
+
+String randomAdviceToJson(RandomAdvice data) => json.encode(data.toJson());
+
+class RandomAdvice {
+    RandomAdvice({
+        required this.slip,
+    });
+
+    Slip slip;
+
+    factory RandomAdvice.fromJson(Map<String, dynamic> json) => RandomAdvice(
+        slip: Slip.fromJson(json["slip"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "slip": slip.toJson(),
+    };
+
+
+
+
+
+  static Future<RandomAdvice> getRandomAdvice() async {
+    var resp = await Dio().get("https://api.adviceslip.com/advice");
+
+    print('passou primeiro');
+    final rvl = RandomAdvice.fromJson(resp.data);
+
+  print('passou segundo');
+    print(rvl);
+    print('passou');
+    return rvl;
+  }
+
+
+
+
+
+}
+
+class Slip {
+    Slip({
+        required this.id,
+        required this.advice,
+    });
+
+    int id;
+    String advice;
+
+    factory Slip.fromJson(Map<String, dynamic> json) => Slip(
+        id: json["id"],
+        advice: json["advice"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "advice": advice,
+    };
+}
+
